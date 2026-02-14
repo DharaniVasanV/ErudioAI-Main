93635a6f897f8fb42adfe294f06e4598b236e14c
DharaniVasanV <dharanivasan.v2024csbs@sece.ac.in>
2026-02-14 14:59:50 +0530

    Fix Android API URL to use emulator host IP

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,9 +3,9 @@ export const config = {
     googleClientId: '830923042304-606in9jgojfj6qmmd7ktn14gc59agu3v.apps.googleusercontent.com'
   },
   android: {
-    googleClientId: '830923042304-m53dbfk67e6bthb9e6hjsjtgt8cffl6u.apps.googleusercontent.com' // Replace after creating Android OAuth client
+    googleClientId: '830923042304-m53dbfk67e6bthb9e6hjsjtgt8cffl6u.apps.googleusercontent.com'
   },
   api: {
-    baseUrl: 'http://localhost:8000' // Change to your production URL when deploying
+    baseUrl: 'http://10.0.2.2:8000' // Android emulator uses 10.0.2.2 to access host machine's localhost
   }
 };
