3bd62311d3b7c44f6a554cd54ab16ab9c46c9f95
DharaniVasanV <dharanivasan.v2024csbs@sece.ac.in>
2026-02-15 14:52:13 +0530

    Add requests dependency for google-auth

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -24,7 +24,7 @@ export const config = {
     googleClientId: '830923042304-606in9jgojfj6qmmd7ktn14gc59agu3v.apps.googleusercontent.com'
   },
   android: {
-    googleClientId: '830923042304-606in9jgojfj6qmmd7ktn14gc59agu3v.apps.googleusercontent.com' // Use same as web
+    googleClientId: '830923042304-m53dbfk67e6bthb9e6hjsjtgt8cffl6u.apps.googleusercontent.com'
   },
   api: {
     baseUrl: getApiBaseUrl()
